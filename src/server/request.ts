6513f95e88c51023f8f7bc40e2de48a66e228082
谴责 <2770894499@qq.com>
2024-03-10 21:29:52 +0800

    💩 动态指定axios-baseUrl

--- src/server/request.ts
+++ src/server/request.ts
@@ -7,10 +7,13 @@ import axios from "axios";
 // 3. 请求拦截器 / 响应拦截器
 
 const request = axios.create({
-  baseURL: "http://127.0.0.1:8080",
+  baseURL: import.meta.env.VITE_SERVER_URL as any,
   timeout: 3000,
-
 });
+
+
+
+
 
 // 请求拦截器
 request.interceptors.request.use(
