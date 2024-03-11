f5cf07886f4578e0f36b969f91f53ed0019a737f
谴责 <2770894499@qq.com>
2024-03-11 17:07:39 +0800

    🐛 修复了验证码溢出的问题

--- src/components/verify/index.vue
+++ src/components/verify/index.vue
@@ -9,8 +9,8 @@ import { reactive, onMounted, ref, toRefs,defineExpose } from "vue";
 
 const verify = ref<any>(null);
 const state: any = reactive({
-    str: "ABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890", // 字符串
-    width: 110,
+    str: "ABCDEFGHiJKLMNoPQRSTUVWXYZ0123456789", // 字符串
+    width: 130,
     height: 35,
     imgCode: "", // 初始化验证码为空
 });
@@ -63,7 +63,7 @@ const draw = () => {
         imgCode += text;
 
         // 设置随机字体大小（18px - 40px）
-        const fontSize = randomNum(18, 40);
+        const fontSize = randomNum(20, 40);
 
         // 设置随机旋转角度（-30度至30度）
         const deg = randomNum(-30, 30);
@@ -87,29 +87,29 @@ const draw = () => {
         ctx.restore();
     }
 
-    // 绘制5条随机干扰线，颜色设置为较浅色系
-    for (let i = 0; i < 5; i++) {
-        ctx.beginPath();
-        ctx.moveTo(randomNum(0, state.width), randomNum(0, state.height));
-        ctx.lineTo(randomNum(0, state.width), randomNum(0, state.height));
-        ctx.strokeStyle = randomColor(180, 230);
-        ctx.closePath(); // 这一行实际上在绘制直线时是可选的
-        ctx.stroke(); // 实际上绘制出干扰线
-    }
+    // // 绘制5条随机干扰线，颜色设置为较浅色系
+    // for (let i = 0; i < 5; i++) {
+    //     ctx.beginPath();
+    //     ctx.moveTo(randomNum(0, state.width), randomNum(0, state.height));
+    //     ctx.lineTo(randomNum(0, state.width), randomNum(0, state.height));
+    //     ctx.strokeStyle = randomColor(180, 230);
+    //     ctx.closePath(); // 这一行实际上在绘制直线时是可选的
+    //     ctx.stroke(); // 实际上绘制出干扰线
+    // }
 
-    // 绘制40个随机位置的小圆点作为干扰元素，颜色同样设置为较浅色系
-    for (let i = 0; i < 40; i++) {
-        ctx.beginPath();
-        // 创建一个圆形路径，指定圆心坐标与半径
-        ctx.arc(randomNum(0, state.width), randomNum(0, state.height), 1, 0, 2 * Math.PI);
-        ctx.closePath(); // 同样，在绘制封闭图形如圆时是可选的
+    // // 绘制40个随机位置的小圆点作为干扰元素，颜色同样设置为较浅色系
+    // for (let i = 0; i < 20; i++) {
+    //     ctx.beginPath();
+    //     // 创建一个圆形路径，指定圆心坐标与半径
+    //     ctx.arc(randomNum(0, state.width), randomNum(0, state.height), 1, 0, 2 * Math.PI);
+    //     ctx.closePath(); // 同样，在绘制封闭图形如圆时是可选的
 
-        // 设置小圆点的颜色
-        ctx.fillStyle = randomColor(150, 200);
+    //     // 设置小圆点的颜色
+    //     ctx.fillStyle = randomColor(150, 200);
 
-        // 填充该圆形路径，形成实心小圆点
-        ctx.fill();
-    }
+    //     // 填充该圆形路径，形成实心小圆点
+    //     ctx.fill();
+    // }
     // 返回生成的验证码字符串
     return imgCode;
 };
@@ -118,7 +118,8 @@ const exposedState = toRefs(state);
 // 暴露方法
 defineExpose({
     draw,
-    exposedState
+    exposedState,
+    handleDraw
 });
 </script>
 <style>
